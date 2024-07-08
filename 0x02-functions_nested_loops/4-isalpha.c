3b1b4afb98fe6ae2e6d655964f08828a12234b52
dike-code <dikeclitonobinna58@gmail.com>
2024-07-08 15:57:08 +0100

    New Commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,16 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: the character to be checked
- * Return: 1 if c is letter, 0 otherwise
+ * _isalpha - check for alphabetic character.
+ *
+ * @c: parameter
+ * Return: Always 0.
  */
-int_isalpha(int c)
+int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
 }
